096c4e80b94e3d907c7394cb606d98077844f8fa
Suvajit2640 <suvajit2003saha@gmail.com>
2025-10-18 01:50:08 +0530

    updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,14 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ToastContainer />
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
